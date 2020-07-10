f887e4fcd35f4942952e7f8714e3d01061a690b1
sjh <275245802@qq.com>
2020-07-10 10:00:32 +0800

    lesson2 finish

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -3,6 +3,8 @@
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include "iostream"
+#include <algorithm>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -14,37 +16,40 @@ const int height = 800;
 
 
 void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color);
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color);
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color) ;
+
+//输入 三角形的3个顶点 待判断点P
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+    //求解u v 1
+    Vec3f u = cross(Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]), Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u[2]) < 1) return Vec3f(-1, 1, 1);
+    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
 
 int main(int argc, char** argv) {
 
-    /*if (2 == argc) {
+
+    if (2 == argc) {
         model = new Model(argv[1]);
     }
     else {
         model = new Model("obj/african_head/african_head.obj");
     }
-
+    
     TGAImage image(width, height, TGAImage::RGB);
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
         for (int j = 0; j < 3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-            line(Vec2i(x0, y0), Vec2i(x1, y1), image, white);
+            Vec3f world_coords = model->vert(face[j]);
+            screen_coords[j] = Vec2i((world_coords.x + 1.) * width / 2., (world_coords.y + 1.) * height / 2.);
         }
-    }*/
-    TGAImage image(width, height, TGAImage::RGB);
-    Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+        Vec2i pts[3] = { screen_coords[0], screen_coords[1], screen_coords[2] };
+        triangle(pts, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+    }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
@@ -85,48 +90,23 @@ void line(Vec2i t0, Vec2i t1,TGAImage& image, TGAColor color) {
     }
 }
 
-/*void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    if (t0.y > t1.y)std::swap(t0, t1);
-    if (t0.y > t2.y)std::swap(t0, t2);
-    if (t1.y > t2.y)std::swap(t1, t2);//三点按照y升序排序
-    int total_height = t2.y - t0.y;
-    int segment_height = t1.y - t0.y + 1;
-    Vec2i tstart = t0, tstop = t1;
-    int flag = 0;
-    for (int i = 0, j = 0; i < total_height; i++, j++) {
-        float alpha = (float)i / total_height;//上升总高度占比
-        float beta = (float)j / segment_height;//上升段高度占比
-        Vec2i A = t0 + (t2 - t0) * alpha;//向t0 t2方向延申
-        Vec2i B = tstart + (tstop - tstart) * beta;//向t0 t1方向延申--->向t1 t2方向延申
-        int x1 = A.x, x2 = B.x;
-        if (x1 > x2)std::swap(x1, x2);
-        for (int x = x1; x <= x2; x++) image.set(x, t0.y + i, color);
-        if (i == segment_height && flag == 0) {
-            //切换为上片段
-            segment_height = t2.y - t1.y + 1;
-            j = 0; tstart = t1; tstop = t2;
-            flag = 1;
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
+    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            bboxmin[j] = std::max(0, std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
         }
     }
-    //image.set(t2.x,t2.y, color);
-}*/
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    if (t0.y == t1.y && t0.y == t2.y) return; // I dont care about degenerate triangles 
-    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
-    if (t0.y > t1.y) std::swap(t0, t1);
-    if (t0.y > t2.y) std::swap(t0, t2);
-    if (t1.y > t2.y) std::swap(t1, t2);
-    int total_height = t2.y - t0.y;
-    for (int i = 0; i < total_height; i++) {
-        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
-        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
-        float alpha = (float)i / total_height;
-        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here 
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
-        if (A.x > B.x) std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++) {
-            image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y 
+    Vec2i P;
+    //遍历bbox中的每一个点
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+            Vec3f bc_screen = barycentric(pts, P);
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+            image.set(P.x, P.y, color);
         }
     }
 }
