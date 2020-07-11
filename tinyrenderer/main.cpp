46ebd6166244641d2901408ac2633d38c818a477
sjh <275245802@qq.com>
2020-07-11 09:19:24 +0800

    L 3 finish

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -8,8 +8,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 128, 0, 255); 
-const TGAColor blue = TGAColor(0, 0, 255, 255);
+const TGAColor green = TGAColor(0, 128, 0, 255);
 
 Model* model = NULL;
 const int width = 800;
@@ -17,10 +16,10 @@ const int height = 800;
 
 
 void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color);
-void triangle(Vec2i* pts, TGAImage& image, TGAColor color) ;
+void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color);
 
 //输入 三角形的3个顶点 待判断点P
-Vec3f barycentric(Vec2i* pts, Vec2i P) {
+Vec3f barycentric(Vec3i* pts, Vec3f P) {
     //求解u v 1
     Vec3f u = cross(Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]), Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
     /* `pts` and `P` has integer value as coordinates
@@ -30,42 +29,54 @@ Vec3f barycentric(Vec2i* pts, Vec2i P) {
     return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
-void rasterize(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color, int ybuffer[]) {
-    if (p0.x > p1.x) {
-        std::swap(p0, p1);//确保从左到右
-    }
-    for (int x = p0.x; x <= p1.x; x++) {
-        float t = (x - p0.x) / (float)(p1.x - p0.x);//从左到右移动的比例
-        int y = p0.y * (1. - t) + p1.y * t;//计算距离y
-        if (ybuffer[x] < y) {
-            ybuffer[x] = y;//用最近的取代
-            for(int i=0;i<16;i++)image.set(x, i, color);
-        }
-    }
-}
 
 int main(int argc, char** argv) {
 
 
-
-    TGAImage render(width, 16, TGAImage::RGB);
-    int ybuffer[width];
-    for (int i = 0; i < width; i++) {
-        ybuffer[i] = std::numeric_limits<int>::min();//填充为最小
+    if (2 == argc) {
+        model = new Model(argv[1]);
     }
-    rasterize(Vec2i(20, 34), Vec2i(744, 400), render, red, ybuffer);
-    rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
-    rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue, ybuffer);
+    else {
+        model = new Model("obj/african_head/african_head.obj");
+    }
 
-    render.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    render.write_tga_file("render.tga");
+    float* zbuffer = new float[width * height];
+    for (int i = 0; i < width * height; i++) {
+        zbuffer[i] = -std::numeric_limits<float>::max();
+    }
+
+
+    TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0, 0, -1);
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec3i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j = 0; j < 3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec3i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.,v.z*100);
+            world_coords[j] = v;
+        }
+        Vec3f n = cross((world_coords[2] - world_coords[0]), (world_coords[1] - world_coords[0]));
+        //猜想 012所对应的3个坐标有一定的顺序，这导致了模型前后的三角形是不同的
+        n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            Vec3i pts[3] = { screen_coords[0], screen_coords[1], screen_coords[2] };
+            triangle(pts, zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        }
+
+    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
     delete model;
     return 0;
 }
 
 
-void line(Vec2i t0, Vec2i t1,TGAImage& image, TGAColor color) {
-    int x0=t0.x, y0=t0.y, x1=t1.x, y1=t1.y;
+void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color) {
+    int x0 = t0.x, y0 = t0.y, x1 = t1.x, y1 = t1.y;
     bool steep = false;
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
@@ -96,7 +107,8 @@ void line(Vec2i t0, Vec2i t1,TGAImage& image, TGAColor color) {
     }
 }
 
-void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
+//加入z坐标
+void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color) {
     Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
     Vec2i bboxmax(0, 0);
     Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
@@ -106,13 +118,19 @@ void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
             bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
         }
     }
-    Vec2i P;
+
+    Vec3f P;
     //遍历bbox中的每一个点
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
             Vec3f bc_screen = barycentric(pts, P);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            image.set(P.x, P.y, color);
+            P.z = 0;
+            for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+            if (zbuffer[int(P.x + P.y * width)] < P.z) {
+                zbuffer[int(P.x + P.y * width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
         }
     }
 }
