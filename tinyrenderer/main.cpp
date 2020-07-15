ffa9103671a8978f0af24abac6271d57594166c4
sjh <275245802@qq.com>
2020-07-15 17:52:13 +0800

    L4 finish

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -6,6 +6,9 @@
 #include "iostream"
 #include <algorithm>
 #include <stdlib.h>
+//#include"geometry.cpp"
+
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -15,14 +18,44 @@ Model* model = NULL;
 Model* model_texture = NULL;
 const int width = 800;
 const int height = 800;
+const int depth = 255;
 int t_w = 0;
 int t_h = 0;
+
+Vec3f camera(0, 0, 3);
 
 
 int main(int argc, char** argv);
 
 void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color);
-void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color, Vec3f* puvs);//加入对应点的材质坐标
+void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color, Vec3f* puvs, float intensity);//加入对应点的材质坐标
+
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
+}
+
+Matrix v2m(Vec3f v) {
+    Matrix m;
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x + w / 2.f;
+    m[1][3] = y + h / 2.f;
+    m[2][3] = depth / 2.f;
+
+    m[0][0] = w / 2.f;
+    m[1][1] = h / 2.f;
+    m[2][2] = depth / 2.f;
+    return m;
+}
+
 
 //输入 三角形的3个顶点 待判断点P
 Vec3f barycentric(Vec3i* pts, Vec3f P) {
@@ -52,6 +85,10 @@ int main(int argc, char** argv) {
         zbuffer[i] = -std::numeric_limits<float>::max();
     }
 
+    Matrix Projection = Matrix::identity(4);
+    Matrix ViewPort = viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+    Projection[3][2] = -1.f / camera.z;
+
 
     TGAImage image(width, height, TGAImage::RGB);
     TGAImage image_t(width, height, TGAImage::RGB);
@@ -68,7 +105,8 @@ int main(int argc, char** argv) {
 
             //std::cout << face[j] << uv.x << " " << uv.y << std::endl;
 
-            screen_coords[j] = Vec3i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2., v.z * 100);
+            //screen_coords[j] = Vec3i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2., v.z * 100);
+            screen_coords[j] = m2v(ViewPort * Projection * v2m(v));
             world_coords[j] = v;
             puvs[j] = Vec3f(uv.x, uv.y, 0.f);
         }
@@ -80,7 +118,7 @@ int main(int argc, char** argv) {
             Vec3i pts[3] = { screen_coords[0], screen_coords[1], screen_coords[2] };
             //int color_r = 255, color_g = 255, color_b = 255;
             //triangle(pts, zbuffer, image, TGAColor(intensity * color_r, intensity * color_g, intensity * color_b, 255));
-            triangle(pts, zbuffer, image, model->diffuse(Vec2f(puvs[0].x, puvs[0].y)), puvs);
+            triangle(pts, zbuffer, image, model->diffuse(Vec2f(puvs[0].x, puvs[0].y)), puvs, intensity);
         }
 
     }
@@ -126,7 +164,10 @@ void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color) {
 }
 
 //加入z坐标
-void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color, Vec3f* puvs) {
+void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color, Vec3f* puvs ,float intensity) {
+
+    if (pts[0].y == pts[1].y && pts[0].y == pts[2].y) return; // i dont care about degenerate triangles
+
     Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
     Vec2i bboxmax(0, 0);
     Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
@@ -154,8 +195,10 @@ void triangle(Vec3i* pts, float* zbuffer, TGAImage& image, TGAColor color, Vec3f
                     uv[0] += puvs[j][0] * bc_screen[j];
                     uv[1] += puvs[j][1] * bc_screen[j];
                 }
+                TGAColor color = model->diffuse(uv);
                 //Vec2f uv = Vec2f(puvs[0].x, puvs[0].y)
-                image.set(P.x, P.y, model->diffuse(uv));
+                image.set(P.x, P.y, TGAColor(color.bgra[2] * intensity, color.bgra[1] * intensity, color.bgra[0] * intensity));
+                //image.set(P.x, P.y, color);
             } 
         }
     }
